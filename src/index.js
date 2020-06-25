8afb68e507faba3cf58e3173da6cb90de8107ccf
HugoRoca <hugo.rock20@hotmail.com>
2020-06-24 23:46:48 -0500

    add crud for notes and add messages with flash

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
 const session = require("express-session");
+const flash = require('connect-flash')
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const Handlebars = require("handlebars");
 const app = express();
 require("./database");
 
@@ -12,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.engine(
   ".hbs",
   exphbs({
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
     defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"),
     partialsDir: path.join(app.get("views"), "partials"),
@@ -30,8 +36,14 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(flash())
 
 // TODO Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 // TODO Routes
 app.use(require("./routes/index"));
